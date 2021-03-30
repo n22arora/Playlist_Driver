d867bbe522c471f70c29c52770f0782ae8bed28f
n22arora <navyaarora0912@outlook.com>
2021-03-30 18:44:05 -0400

    Update listentry.cpp

--- listentry.cpp
+++ listentry.cpp
@@ -2,31 +2,6 @@
 //  listentry.cpp
 //  Project_0_250
 //
-//  Created by Himanshi Gidwani on 2021-01-26.
-//
-
-/*
-
-song artist
-
-constructor
-
-getter
-
-isequal
-
-case insensitive
-
-boost
-
-transform
-
-*/
-
-
-
-
-
 #include <iostream>
 
 using namespace std;
